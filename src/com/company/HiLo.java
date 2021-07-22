a4b6d7ef87bf428d24ca2d2a56ae530963eed28e
edwin2017 <edwin-galustov@mail.ru>
2021-07-22 17:52:00 +0300

    First complete game

---
+++ src/com/company/HiLo.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class HiLo {
+
+    public static void main(String... args) {
+        Scanner scan = new Scanner(System.in);
+// Генерация случайного числа
+        int theNumber = (int)(Math.random()*100+1);
+//        System.out.println(theNumber);
+        int guess;
+        int count = 0;
+        do {
+            System.out.println("Guess the Number (1...100):");
+            guess = scan.nextInt();
+            System.out.println("You guess " + guess);
+            if (guess > theNumber)
+                System.out.println("try smaller");
+            else if(guess<theNumber)
+                System.out.println("try bigger");
+            else if(guess == theNumber)
+                System.out.println("Congratulations! You win!!! :)");
+            count++;
+        } while (guess != theNumber);
+        System.out.println("You have used " + count + " number of trying!");
+
+    }
+
+
+}
